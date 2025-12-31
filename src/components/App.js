f0f5e605e054c1b8f44c4c6f90c7839f156c8b46
AdityaGaikwad03 <gaikwadaditya0309@gmail.com>
2025-12-31 13:18:03 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deploy the React app", completed: false }
+  ]);
 
-const App = () => {
+  function handleComplete(id) {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Todo List</h2>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function TodoList({ todos, handleComplete }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+
+          {todo.completed ? (
+            <></>
+          ) : (
+            <button onClick={() => handleComplete(todo.id)}>
+              Complete
+            </button>
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
